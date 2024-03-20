6949fcaeb8aae5ac57fc5f0ea046c2d2040fc357
Suleman <slmsqb9@gmail.com>
2024-03-20 20:43:27 +1100

    undid yide changes entirely so __transform__ occurs before __formatOutput__

--- galaxydatasetv2.py
+++ galaxydatasetv2.py
@@ -7,6 +7,7 @@ from pycocotools.coco import COCO
 
 import torch
 from torch.utils.data import Dataset
+from torchvision.ops import box_area
 from torchvision.io import read_image
 from torchvision.tv_tensors import Mask, BoundingBoxes
 
@@ -64,8 +65,8 @@ class RadioGalaxyNET(Dataset):
         """
         idx = idx.tolist() if torch.is_tensor(idx) else idx
         imgId = self.ids[idx] # corresponding imgId
-        img, boxes, instanceMasks, labels, iscrowd, area = self.__id2item__(imgId)
-        img, boxes, instanceMasks, area = self.__transform__(img, boxes, instanceMasks, area)
+        img, boxes, instanceMasks, labels, iscrowd = self.__id2item__(imgId)
+        img, boxes, instanceMasks, area = self.__transform__(img, boxes, instanceMasks)
         return self.__formatOutput__(imgId, img, boxes, instanceMasks, labels, iscrowd, area)
     
     def __formatOutput__(self, imgId, img, boxes, instanceMasks, labels, iscrowd, area):
@@ -94,7 +95,7 @@ class RadioGalaxyNET(Dataset):
                          'iscrowd': iscrowd}
         return img, my_annotation
         
-    def __transform__(self, img, boxes, instanceMasks, area):
+    def __transform__(self, img, boxes, instanceMasks):
         """
         Apply transformations to the image, boxes, and masks.
 
@@ -108,11 +109,12 @@ class RadioGalaxyNET(Dataset):
 
         """
         if self.transforms is not None:
-            img, boxes, instanceMasks, area = self.transforms(img, boxes, instanceMasks, area)
+            img, boxes, instanceMasks = self.transforms(img, boxes, instanceMasks)
         
         if self.transform is not None:
             img = self.transform(img)
         
+        area = box_area(boxes)
         return img, boxes, instanceMasks, area
 
     def __id2item__(self, imgId):
@@ -132,10 +134,10 @@ class RadioGalaxyNET(Dataset):
 
         annIds = self.coco.getAnnIds(imgId)
         anns = self.coco.loadAnns(annIds)
-        boxes, instanceMasks, labels, area = self.__annToTarget__(anns)
+        boxes, instanceMasks, labels = self.__annToTarget__(anns)
         
         iscrowd = torch.zeros((len(anns),), dtype=torch.int64) #  [Mod: Yide 20/03/2024]: iscrowd not important for our case
-        return img, boxes, instanceMasks, labels, iscrowd, area
+        return img, boxes, instanceMasks, labels, iscrowd
     
     def __annToTarget__(self, anns):
         """
@@ -153,10 +155,9 @@ class RadioGalaxyNET(Dataset):
                  ann['bbox'][1] + ann['bbox'][3]] for ann in anns]
         bbox = BoundingBoxes(bbox, format='XYXY', canvas_size=(self.h, self.w))
         
-        area = torch.tensor([ann['area'] for ann in anns], dtype=torch.float32)
         labels = torch.tensor([ann['category_id'] for ann in anns], dtype=torch.int64)
         masks = Mask(np.array([self.coco.annToMask(ann) for ann in anns]))
-        return bbox, masks, labels, area
+        return bbox, masks, labels
 
     def __instance2semantic__(self, instanceMasks, labels):
         """
